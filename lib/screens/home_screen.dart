36a16227c52b479739de4d9bc88b0f5a16ff7350
Carlo10K <carlosortegadiaz50@gmail.com>
2024-04-15 18:41:02 -0600

    card slider

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,7 +13,15 @@ class HomeScreen extends StatelessWidget {
           IconButton(onPressed: () {}, icon: const Icon(Icons.search_outlined))
         ],
       ),
-      body: const Column(children: [CardSwiper()]),
+      body: const SingleChildScrollView(
+        child: Column(children: [
+          //Swiper
+          CardSwiper(),
+
+          //Slider
+          MovieSlider()
+        ]),
+      ),
     );
   }
 }
