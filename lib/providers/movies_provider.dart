8a7611bd672e766ea9850f754307a5def825adc3
Carlo10K <carlosortegadiaz50@gmail.com>
2024-05-12 16:32:25 -0600

    motor de busqueda

--- lib/providers/movies_provider.dart
+++ lib/providers/movies_provider.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 import 'package:peliculas_app/models/models.dart';
+import 'package:peliculas_app/models/search_response.dart';
 
 class MoviesProvider extends ChangeNotifier {
   String _apiKey = dotenv.get('API_KEY');
@@ -19,10 +20,10 @@ class MoviesProvider extends ChangeNotifier {
   }
 
   Future<String> _getJsonData(String endpoint, [int page = 1]) async {
-    var url = Uri.https(_baseUrl, endpoint,
+    final url = Uri.https(_baseUrl, endpoint,
         {'api_key': _apiKey, 'language': _language, 'page': '$page'});
 
-    var response = await http.get(url);
+    final response = await http.get(url);
     return response.body;
   }
 
@@ -51,5 +52,15 @@ class MoviesProvider extends ChangeNotifier {
     moviesCast[movieId] = creditsResponse.cast;
 
     return creditsResponse.cast;
+  }
+
+  Future<List<Movie>> searchMovie(String query) async {
+    final url = Uri.https(_baseUrl, '3/search/movie',
+        {'api_key': _apiKey, 'language': _language, 'query': query});
+
+    final response = await http.get(url);
+    final searchResponse = SearchResponse.fromJson(response.body);
+
+    return searchResponse.results;
   }
 }
